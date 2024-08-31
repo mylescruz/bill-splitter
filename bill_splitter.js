d4079d4d353d273dfb06e54e2ff31380099189cf
Myles <mylesvirgilcruz@gmail.com>
2024-08-31 11:41:18 -0700

    Add functionality for splitting the bill evenly

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,16 +1,22 @@
-const hideWelcome = function() {
-    document.querySelector(".welcome").style.display = "none";
+const hideWelcome = () => {
+    document.querySelector('.welcome').style.display = 'none';
 };
 
-window.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("even-btn").addEventListener("click", () => {
-        console.log("Even");
-        document.querySelector(".even-split").style.display = "flex";
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('even-btn').addEventListener('click', () => {
         hideWelcome();
+        document.querySelector('.even-split').style.display = 'flex';
+        
+        document.getElementById('split-btn').addEventListener('click', () => {
+            const billTotal = document.getElementById('split-total').value;
+            const numPeople = document.getElementById('num-people').value;
+
+            const splitAmount = billTotal / numPeople;
+            document.querySelector('.split-amount').textContent = `$${splitAmount.toFixed(2)}`;
+        });
     });
-    document.getElementById("item-btn").addEventListener("click", () => {
-        console.log("Item");
-        document.querySelector(".item-split").style.display = "flex";
+    document.getElementById('item-btn').addEventListener('click', () => {
         hideWelcome();
+        document.querySelector('.item-split').style.display = 'flex';
     });
 });
