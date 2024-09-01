da3ff5eff1827285521c9767cdd07400475f83b7
Myles <mylesvirgilcruz@gmail.com>
2024-09-01 15:13:04 -0700

    Add ability to input diners to split a bill by item

--- bill_splitter.js
+++ bill_splitter.js
@@ -6,6 +6,53 @@ const hideWelcome = () => {
     document.querySelector('.welcome').style.display = 'none';
 };
 
+const diners = [];
+
+const addDiners = () => {
+    const peopleContainer = document.querySelector('.addPeople');
+    const nameInput = document.getElementById('diner-name');
+
+    diners.push(nameInput.value);
+    console.log(nameInput.value);
+    console.log(diners);
+    
+    const diner = document.createElement('p');
+    diner.innerHTML = nameInput.value;
+    nameInput.value='';
+    peopleContainer.append(diner);
+}
+
+const inputDiners = () => {
+    const nameInput = document.getElementById('diner-name');
+    const addButton = document.getElementById('add-diner-btn');
+    const nextButton = document.getElementById('next-btn');
+    
+    addButton.addEventListener('click', () => {
+        if (nameInput.value !== '') {
+            addDiners();
+        }
+
+        if (diners.length > 0) {
+            nextButton.removeAttribute('disabled');
+        }
+    });
+    nameInput.addEventListener('keydown', (event) => {
+        if (nameInput.value !== '' && event.key === 'Enter') {
+            addDiners();
+        }
+
+        if (diners.length > 0) {
+            nextButton.removeAttribute('disabled');
+        }
+    });
+
+    
+    
+    nextButton.addEventListener('click', () => {
+        console.log("Next screen");
+    });
+};
+
 window.addEventListener('DOMContentLoaded', () => {
     document.getElementById('even-btn').addEventListener('click', () => {
         hideWelcome();
@@ -23,7 +70,7 @@ window.addEventListener('DOMContentLoaded', () => {
         hideWelcome();
         document.querySelector('.item-split').style.display = 'flex';
 
-        addDiners();
+        inputDiners();
     });
     document.getElementById('even-home-btn').addEventListener('click', () => {
         document.querySelector('.even-split').style.display = 'none';
